a5cd9358ccbabd79c512d80a675165efe50d0f4b
Renan Gomes <renangomesalves28@gmail.com>
2026-05-15 12:10:04 -0300

    Fix image paths for menu button in main.js
    Updated image source paths for menu button.

--- JS/main.js
+++ JS/main.js
@@ -3,15 +3,13 @@ const form = {
     btnMenu: () => document.querySelector("#btn-menu")
 }
 
-form.btnMenu().src = "./IMG/menu.svg"
-
 const toggleMenu = () => {
     if (form.sidebar().style.display === "flex") {
         form.sidebar().style.display = "none"
-        form.btnMenu().src = "./IMG/menu.svg"
+        form.btnMenu().src = "IMG/menu.svg"
     } else {
         form.sidebar().style.display = "flex"
-        form.btnMenu().src = "./IMG/close.svg"
+        form.btnMenu().src = "IMG/close.svg"
     }
 }
 
