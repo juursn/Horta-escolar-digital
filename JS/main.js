e7b760f5aa1b28fc833025f60d853cef31e2e559
Ana Júlia <anajuliaramos343@gmail.com>
2026-05-15 11:38:41 -0300

    Update image paths for menu button

--- JS/main.js
+++ JS/main.js
@@ -6,10 +6,10 @@ const form = {
 const toggleMenu = () => {
     if (form.sidebar().style.display === "flex") {
         form.sidebar().style.display = "none"
-        form.btnMenu().src = "IMG/menu.svg"
+        form.btnMenu().src = "../IMG/menu.svg"
     } else {
         form.sidebar().style.display = "flex"
-        form.btnMenu().src = "IMG/close.svg"
+        form.btnMenu().src = "../IMG/close.svg"
     }
 }
 
